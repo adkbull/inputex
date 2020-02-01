37a47ee1feca230934b04dd30840e0380a0d70db
adkbull <59900398+adkbull@users.noreply.github.com>
2020-01-31 22:01:59 -0500

    Add files via upload

---
+++ UserInputPeterAdkins/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserInputPeterAdkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi this is Peter Adkins");
+            Console.WriteLine("type whatever you like then press enter");
+            string rmessage = Console.ReadLine();
+            Console.WriteLine("message" + " " + rmessage + " " + "received!");
+
+        }
+    }
+}
